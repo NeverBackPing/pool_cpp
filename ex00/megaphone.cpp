82638a5f7a8cee0b394f5a1f281c85dc5b73e24e
Stevenson Jossaint <neverbackping@gmail.com>
2025-02-25 16:28:14 +0100

    ex00 done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+#include <cctype>
 
-int	main()
+void	ft_megaphone(char **str)
 {
+	int		i;
+	int		j;
+	char	c;
 
+	i = 1;
+	while (str[i])
+	{
+		j = 0;
+		while (str[i][j])
+		{
+			if (isalpha(str[i][j]))
+			{
+				c = toupper(str[i][j]);
+				std::cout << c;
+			}
+			else
+				std::cout << str[i][j];
+			j++;
+
+		}
+		std::cout << " ";
+		i++;
+	}
+	std::cout << "\n";
+}
+
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (-1);
+	}
+	ft_megaphone(av);
+	return (0);
 }
