1241991ff1390370a0d5f63b5ba5aec682a6621e
NeverBackPing <neverbackping@gmail.com>
2025-03-20 15:20:46 +0100

    add test and time

--- ex02/srcs/Account.cpp
+++ ex02/srcs/Account.cpp
@@ -1,1 +1,29 @@
 #include "Account.hpp"
+
+void	Account::_displayTimestamp(void)
+{
+	tm		*ltm;
+	time_t	now;
+
+	now = time(0);
+	ltm = localtime(&now);
+	std::cout << "[";
+	std::cout << 1900 + ltm->tm_year << 1 + ltm->tm_mon << ltm->tm_mday;
+	std::cout << "_" ;
+	std::cout << 0 + ltm->tm_hour << 0 + ltm->tm_min << ltm->tm_sec ;
+	std::cout << "]" ;
+}
+
+void	Account::displayStatus(void)const
+{
+	std::cout << "index:" << _accountIndex << ";";
+	std::cout << "amount:" << _amount << ";";
+	std::cout << "created" << std::endl;
+
+	}
+
+void	Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+
+}
