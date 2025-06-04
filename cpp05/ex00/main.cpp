0c75be013aad447b3b98e80bd1801258ffa79d5d
Stevenson Jossaint <neverbackping@gmail.com>
2025-06-04 15:49:02 +0200

    finish

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -5,15 +5,15 @@ int	main(void)
 	Bureaucrat employed("Kevin", 2);
 	Bureaucrat employed2;
 
-	std::cout << "[ Emplyed 1 ]" << std::endl << std::endl;
+	std::cout << std::endl <<"\033[31m[ Emplyed 1 ]\033[0m" << std::endl << std::endl;
 
 	std::cout << employed << std::endl;
 
-	std::cout << "[ Emplyed 2 ]" << std::endl << std::endl;
+	std::cout << "\033[31m[ Emplyed 2 ]\033[0m" << std::endl << std::endl;
 
 	std::cout << employed2 << std::endl;
 
-	std::cout << "[ Edit Emplyed 2 ]" << std::endl << std::endl;
+	std::cout << "\033[31m[ Edit Emplyed 2 ]\033[0m" << std::endl << std::endl;
 
 	employed2 = employed;
 
