de3d3a02e2d57de7dbaebffcafd649a3c16a89f6
NeverBackPing <neverbackping@gmail.com>
2025-08-05 01:10:57 +0200

    start ex02

--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -1,8 +1,9 @@
-#include "../include/Array.hpp"
+#include <iostream>
+#include <cstdlib>
+#include "Array.hpp"
 
 #define MAX_VAL 750
-
-int main(void)
+int main(int, char**)
 {
     Array<int> numbers(MAX_VAL);
     int* mirror = new int[MAX_VAL];
@@ -15,8 +16,46 @@ int main(void)
     }
     //SCOPE
     {
-        Array<int> tmp = numbers;
+        Array<int> tmp;
+        try
+        {
+            for (int i = 0; i < MAX_VAL + 1; i++)
+                std::cout << "tmp[" << i << "]:\t" << tmp[i] << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\t';
+            std::cerr << "You tried to access past the last element of the array" << std::endl;
+        }
+        tmp = numbers;
         Array<int> test(tmp);
+
+        try
+        {
+            for (int i = 0; i < MAX_VAL + 1; i++)
+        {
+            if (tmp[i] != test[i])
+            {
+                std::cerr << "didn't save the same value!!" << std::endl;
+                return 1;
+            }
+            // std::cout << "tmp[" << i << "]:\t" << tmp[i] << std::endl; //uncomment these two lines to see the arrays have the same values
+            // std::cout << "test[" << i << "]:\t" << test[i] << std::endl;
+        }
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\t';
+            std::cerr << "You tried to access past the last element of the array" << std::endl;
+        }
+
+        std::cout << std::endl << "test[1]:\t" << test[1] << std::endl;
+        std::cout << "tmp[1]:\t\t" << tmp[1] << std::endl << std::endl;
+
+        test[1] = 123456789;
+
+        std::cout << "test[1]:\t" << test[1] << std::endl;
+        std::cout << "tmp[1]:\t\t" << tmp[1] << std::endl << std::endl;
     }
 
     for (int i = 0; i < MAX_VAL; i++)
@@ -34,6 +73,7 @@ int main(void)
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
+        // std::cout << "index was -2" << std::endl;
     }
     try
     {
@@ -42,6 +82,7 @@ int main(void)
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
+        // std::cout << "tried to access past the last element of the array" << std::endl;
     }
 
     for (int i = 0; i < MAX_VAL; i++)
@@ -49,5 +90,9 @@ int main(void)
         numbers[i] = rand();
     }
     delete [] mirror;//
+    // for (int i = 0; i < MAX_VAL; i++)
+    // {
+    //     std::cout << numbers[i] << std::endl;
+    // }
     return 0;
 }
